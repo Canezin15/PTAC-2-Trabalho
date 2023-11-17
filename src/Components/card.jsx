35188989608121e524aab88c647611ed5fb09683
Canezin15 <jp1603207@gmail.com>
2023-11-17 14:13:21 -0400

    commit

---
+++ src/Components/card.jsx
@@ -0,0 +1,8 @@
+export default function card({atividade}){
+    return (
+        <div>
+            <h1>{atividade.atividade}</h1>
+        </div>
+    )
+
+}
--- src/Detalhe/detalhe.jsx
+++ src/Detalhe/detalhe.jsx
@@ -1,12 +1,22 @@
-import { useParams } from "react-router-dom";
-    
-    export default function Detalhe() {
-        const { id } = useParams ();
+    import { useParams } from "react-router-dom";
+    import card from './Components/card';
 
+
+    export default function Detalhe() {
+        
+        const { id } = useParams ();
+        const lista = JSON.parse (localStorage.getItem("Lista"))
+        
+       const atividade = lista.filter((objeto) => {
+            if(objeto.id == id){
+                return objeto;
+            }
+            return null;
+        })
+        
         return (
-            <div>
-                <h1>Ola mindo</h1>
-            </div>
-        )
+
+                <card atividade={atividade[0] } />
+        );
     }
  
